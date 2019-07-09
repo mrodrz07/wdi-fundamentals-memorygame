729e1358248071bea615d5368592720288814f60
Joshua <joshua@Carlas-MacBook-Air.local>
2019-07-09 18:59:47 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Hey Marty :)");
+
+var cardOne = 'queen';
+var cardTwo = 'king';
+var cardThree = 'queen';
+var cardFour = 'king';
+// Variables have been declared for all four cards
+
+console.log('User flipped ' + cardOne);
