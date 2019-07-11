7e5e9e5295c29fb6170d750557dc8664351ed839
Joshua <joshua@Carlas-MacBook-Air.local>
2019-07-11 00:29:14 -0400

    created additional js logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,44 @@
 console.log("Hey Marty :)");
-
+/*
 var cardOne = 'queen';
 var cardTwo = 'king';
 var cardThree = 'queen';
 var cardFour = 'king';
-// Variables have been declared for all four cards
+- these variables are no longer needed */
 
-console.log('User flipped ' + cardOne);
+
+
+// alert('You found a match!'); //testing alerts
+
+var cards = ['queen', 'king', 'queen', 'king']; 
+// array storing values for each of the 4 cards
+
+var cardsInPlay = [];
+// will hold cards that are in play
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardThree = cards[2];
+var cardFour = cards[3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+cardsInPlay.push(cardThree);
+cardsInPlay.push(cardFour);
+//I've pushed the variables into cardsInPlay array
+
+console.log('User flipped' + ' ' + cardOne);
+console.log('User flipped' + ' ' + cardTwo);
+console.log('User flipped' + ' ' + cardThree);
+console.log('User flipped' + ' ' + cardFour);
+// this will display a message for all four cards
+
+if(cardsInPlay.length === 2) {
+	console.log('yes');
+} if(cardsInPlay[0] === cardsInPlay[2]) {
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
+// substitute the index in either cardsInPlay arrays
+
